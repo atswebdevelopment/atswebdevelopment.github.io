84457947c6bd4474a96efa01d52b0a889c784564
Adam Southorn <adam@atswebdevelopment.com>
2021-07-08 21:37:23 +0100

    Update believe

--- scripts/js/dev.js
+++ scripts/js/dev.js
@@ -1,49 +1,13 @@
-// let imagesWhyReached = false
-// let whoAreWeReached = false
-// let quotesReached = false
-// let imagesWorkReached = false
 
 document.addEventListener('scroll', () => {
-  // if (!imagesWhyReached) {
-  //   const imagesWhy = document.querySelector('.images-why');
-  //   const imagesWhyOffset = imagesWhy.getBoundingClientRect().top
-  //   const imagesWhyHeight = window.innerHeight / 10
-  //   if (imagesWhyOffset - window.innerHeight + imagesWhyHeight < 0) {
-  //     document.querySelector('.images-why').classList.add('active')
-  //     imagesWhyReached = true
-  //   }
-  // } else if (!whoAreWeReached) {
-  //   const whoAreWe = document.querySelector('.who-are-we');
-  //   const whoAreWeOffset = whoAreWe.getBoundingClientRect().top
-  //   const whoAreWeHeight = window.innerHeight / 10
-  //   if (whoAreWeOffset - window.innerHeight + whoAreWeHeight < 0) {
-  //     document.querySelector('.who-are-we').classList.add('active')
-  //     whoAreWeReached = true
-  //   }
-  // } else if (!quotesReached) {
-  //   const quotes = document.querySelector('.quote');
-  //   const quotesOffset = quotes.getBoundingClientRect().top
-  //   const quotesHeight = window.innerHeight / 10
-  //   if (quotesOffset - window.innerHeight + quotesHeight < 0) {
-  //     document.querySelector('.quote').classList.add('active')
-  //     quotesReached = true
-  //   }
-  // } else if (!imagesWorkReached) {
-  //   const imagesWork = document.querySelector('.work-here');
-  //   const imagesWorkOffset = imagesWork.getBoundingClientRect().top
-  //   const imagesWorkHeight = window.innerHeight / 10
-  //   if (imagesWorkOffset - window.innerHeight + imagesWorkHeight < 0) {
-  //     document.querySelector('.work-here').classList.add('active')
-  //     imagesWorkReached = true
-  //   }
-  // }
-
   const header = document.querySelector('.container .header')
-  if(window.scrollY > 320) {
-    header.classList.add('header--white')
-  }
-  else {
-    header.classList.remove('header--white')
+  if (window.innerWidth > 985) {
+    if(window.scrollY > 320) {
+      header.classList.add('header--white')
+    }
+    else {
+      header.classList.remove('header--white')
+    }
   }
 })
 
@@ -65,35 +29,23 @@ if (burgerClose) {
   })
 }
 
-// const toViewRoles = () => {
-//   setTimeout(() => {
-//     const positions = document.querySelector('.positions');
-//     let positionsOffset = positions.getBoundingClientRect().top
-//     if (window.innerWidth < 767) {
-//       positionsOffset += 100
-//     }
-//     if (positionsOffset > 110) {
-//       window.scrollTo(0, window.scrollY + (positionsOffset / 50))
-//       toViewRoles()
-//     }
-//   }, 1)
-// }
+const toLink = (e) => {
+  setTimeout(() => {
+    const positions = document.querySelector('.' + e);
+    let positionsOffset = positions.getBoundingClientRect().top - 150
+    if ((positionsOffset / 50) > 1 || (positionsOffset / 50) < -1) {
+      window.scrollTo(0, window.scrollY + (positionsOffset / 50))
+      toLink(e)
+    }
+  }, 1)
+}
 
-// const toViewRolesLink = document.querySelector('.header__cta a')
-// toViewRolesLink.addEventListener('click', () => {
-//   toViewRoles()
-// })
-
-// const accordionContent = document.querySelectorAll('.accordion__content')
-// accordionContent.forEach((accordion) => {
-//   accordion.height = accordion.offsetHeight + 'px'
-//   accordion.style.maxHeight = '0px'
-// })
-
-// const accordionHeader = document.querySelectorAll('.accordion__header')
-// accordionHeader.forEach((accordion, i) => {
-//   accordion.addEventListener('click', () => {
-//     accordionContent[i].style.maxHeight = accordionContent[i].style.maxHeight === '0px' ? accordionContent[i].height : '0px'
-//     accordion.parentElement.classList.toggle('active')
-//   })
-// })
+const headerLinks = document.querySelectorAll('.header__link a')
+headerLinks.forEach((headerLink, i) => {
+  headerLink.addEventListener('click', (e) => {
+    e.preventDefault()
+    toLink(e.target.getAttribute('data-val'))
+    const body = document.querySelector('body')
+    body.classList.remove('main-nav-active')
+  })
+})
